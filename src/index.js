c3ce624bd2ea6d8fdf037df3888c1a6acc74f999
buihongquan135 <19522073@gm.uit.edu.vn>
2023-03-14 09:39:01 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,99 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {useState} from "react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+
+function Calculator() {
+    const [result, setResult] = useState("");
+
+    function handleNumOpe(event) {
+        setResult(result.concat(event.target.value))
+    }
+
+    function handleEqual() {
+        try {
+            setResult(String(eval(result)))
+        } catch (err) {
+            setResult("ERROR")
+        }
+    }
+
+    function handleDEL() {
+        setResult(result.substring(0, result.length - 1))
+    }
+
+    function handleAC() {
+        setResult("")
+    }
+
+
+    return (
+        <div className="calculator-grid">
+            <div className="output">
+                <div className="previous-operand"></div>
+                <div className="current-operand">{result}</div>
+            </div>
+            <button className="span-two"
+                    onClick={handleAC}>AC
+            </button>
+            <button onClick={handleDEL}>DEL</button>
+            <button onClick={handleNumOpe}
+                    value={'/'}>/
+            </button>
+            <button onClick={handleNumOpe}
+                    value={'1'}>1
+            </button>
+            <button onClick={handleNumOpe}
+                    value={'2'}>2
+            </button>
+            <button onClick={handleNumOpe}
+                    value={'3'}>3
+            </button>
+            <button onClick={handleNumOpe}
+                    value={'*'}>*
+            </button>
+            <button onClick={handleNumOpe}
+                    value={'4'}>4
+            </button>
+            <button onClick={handleNumOpe}
+                    value={'5'}>5
+            </button>
+            <button onClick={handleNumOpe}
+                    value={'6'}>6
+            </button>
+            <button onClick={handleNumOpe}
+                    value={'+'}>+
+            </button>
+            <button onClick={handleNumOpe}
+                    value={'7'}>7
+            </button>
+            <button onClick={handleNumOpe}
+                    value={'8'}>8
+            </button>
+            <button onClick={handleNumOpe}
+                    value={'9'}>9
+            </button>
+            <button onClick={handleNumOpe}
+                    value={'-'}>-
+            </button>
+            <button onClick={handleNumOpe}
+                    value={'.'}>.
+            </button>
+            <button onClick={handleNumOpe}
+                    value={'0'}>0
+            </button>
+            <button className="span-two"
+                    onClick={handleEqual}>=
+            </button>
+        </div>
+    );
+}
+
+root.render(<Calculator/>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
